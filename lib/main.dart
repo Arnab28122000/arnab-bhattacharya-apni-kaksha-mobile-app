7134c19a5e36a01eeaab9e1babcc46856c584278
Arnab <arnab@felicity.care>
2021-06-25 06:37:58 +0530

    removed debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Apni Kaksha',
       theme: ThemeData(
         primarySwatch: Colors.blue,
